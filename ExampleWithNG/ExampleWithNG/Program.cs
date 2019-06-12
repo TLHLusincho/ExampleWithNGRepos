41f05b1a4f75b44266c4e452db70566a4bf8a102
lusine.torosyan <lusine.torosyan@helpsystems.com>
2019-06-12 12:53:52 +0400

    Project first commit.

---
+++ ExampleWithNG/ExampleWithNG/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace ExampleWithNG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pix img = Pix.LoadFromFile("images/i1.jpg");
+            TesseractEngine engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
+            Page page = engine.Process(img, PageSegMode.Auto);
+            string result = page.GetText();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(result);
+        }
+    }
+}
